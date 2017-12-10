d650db90964265b29d6a4684ae4f2bbedae8e186
dportok <dportok@gmail.com>
2017-12-10 21:45:50 +0000

    [Task]: Delete directories

--- terraform/modules/network_setup/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id_out" {
-  description = "The ID of the VPC"
-  value       = "${aws_vpc.vpc_demo.id}"
-}
-
-output "public_subnet_id_out" {
-  description = "The ID of the Public subnet"
-  value       = "${aws_subnet.public-subnet.id}"
-}
--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip" {
-  value = "${aws_instance.ubuntu-instance.public_ip}"
-}

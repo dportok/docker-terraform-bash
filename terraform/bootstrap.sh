663be41a2fae2d507200e2937e55c6d83abbe54e
dportok <dportok@gmail.com>
2017-12-10 13:39:47 +0000

    [Task]: Add README file for terraform

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 rm -rf .terraform && rm -rf terraform.tfstate*
+
 aws-vault exec personal -- terraform get
+if [ $? -eq 0 ]; then
+    echo "Terraform get was successful"
+else
+    echo "Terraform get failed"
+    echo "Exiting script..."
+    exit 1
+fi
 aws-vault exec personal -- terraform init
+if [ $? -eq 0 ]; then
+    echo "Terraform init was successful"
+else
+    echo "Terraform init failed"
+    echo "Exiting script..."
+    exit 1
+fi
+
 aws-vault exec personal -- terraform apply
+if [ $? -eq 0 ]; then
+    echo "Terraform build was successful"
+else
+    echo "Terraform build failed"
+    echo "Exiting script..."
+    exit 1
+fi
+
 sleep 2m
 
 cat << EOF >> backend.tf 
